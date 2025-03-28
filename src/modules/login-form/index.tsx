c4e40fb8935f4894311b3d89df397ea4a9a62a5f
Darya Korbut <kitkatlolipop@mail.ru>
2025-03-28 13:21:49 +0300

    feat: add login-form layout

---
+++ src/modules/login-form/index.tsx
@@ -0,0 +1,81 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { Box, Button, Typography } from '@mui/material';
+import { useLogin } from '@api/hooks/auth';
+import { Credentials } from '@api/types';
+import { FormField } from '@components/index';
+import { setUser } from '@store/slices/user-slice';
+import styles from './styles.module.scss';
+import { LoginFormFields } from './types';
+import PasswordField from '@src/components/password-field';
+
+const LoginForm = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { error, user, loginUser } = useLogin();
+  const {
+    handleSubmit,
+    control,
+    formState: { errors, isSubmitting },
+  } = useForm<LoginFormFields>({
+    defaultValues: {
+      username: '',
+      password: '',
+    },
+  });
+
+  useEffect(() => {
+    if (user) {
+      dispatch(setUser(user));
+      navigate('/');
+    }
+  }, [user, navigate, dispatch]);
+
+  const onSubmit: SubmitHandler<Credentials> = async (data) => {
+    await loginUser(data);
+  };
+
+  return (
+    <Box
+      component={'form'}
+      onSubmit={handleSubmit(onSubmit)}
+      className={styles.form}
+    >
+      <Typography variant="h4" component="h1" gutterBottom>
+        Sign in
+      </Typography>
+      {error && (
+        <Typography color="error" sx={{ mb: 2 }}>
+          {error}
+        </Typography>
+      )}
+      <FormField
+        name="username"
+        label="Username"
+        type="text"
+        control={control}
+        error={!!errors.username}
+        helperText={errors.username?.message}
+      />
+      <PasswordField
+        name="password"
+        label="Password"
+        control={control}
+        error={!!errors.password}
+        helperText={errors.password?.message}
+      />
+      <Button
+        type="submit"
+        variant="contained"
+        loading={isSubmitting}
+        disabled={isSubmitting || !!Object.keys(errors).length}
+      >
+        Sign in
+      </Button>
+    </Box>
+  );
+};
+
+export default LoginForm;
