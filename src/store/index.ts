7d1636572a66ddb9c3300ca5da8c7296abb333a2
Darya Korbut <kitkatlolipop@mail.ru>
2025-03-28 13:16:43 +0300

    feat: add user reducer

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
-import menuReducer from './slices/menu-slice';
+import { menuReducer, userReducer } from './slices';
 
 export const store = configureStore({
   reducer: {
     menu: menuReducer,
+    user: userReducer,
   },
 });
 
