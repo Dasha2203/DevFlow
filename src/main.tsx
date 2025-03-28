d214e6eb17d9ff36f6323619874c3bfbca43a19e
Darya Korbut <kitkatlolipop@mail.ru>
2025-03-28 13:22:52 +0300

    feat: add redux and router providers to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,22 @@
-import { StrictMode } from 'react';
+import Register from '@src/pages/register';
 import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
+import { Provider } from 'react-redux';
+import { BrowserRouter, Route, Routes } from 'react-router';
+import Layout from './layout';
+import Login from './pages/login';
+import { store } from './store';
+import './index.scss';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  <Provider store={store}>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<div>main page</div>} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </Provider>
 );
