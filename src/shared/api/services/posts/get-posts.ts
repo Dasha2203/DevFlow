996ce059965f8cee3556454decd697283726cd7a
Darya Korbut <kitkatlolipop@mail.ru>
2025-04-01 22:45:36 +0300

    feat: add post's services

---
+++ src/shared/api/services/posts/get-posts.ts
@@ -0,0 +1,35 @@
+import { api } from '@api/api';
+import { GetPostsParams, PostsResponse } from '@api/types/posts';
+
+export const getPosts = async ({
+  page,
+  limit,
+  sortBy,
+  search,
+}: GetPostsParams = {}): Promise<PostsResponse> => {
+  try {
+    const params = new URLSearchParams();
+
+    const queryParams: Record<string, string | undefined> = {
+      page: page ? String(page) : undefined,
+      limit: limit ? String(limit) : undefined,
+      sortBy,
+      search,
+    };
+
+    Object.entries(queryParams).forEach(([key, value]) => {
+      if (value) {
+        params.set(key, value);
+      }
+    });
+
+    const { data } = await api.get<PostsResponse>('/snippets', {
+      params,
+    });
+
+    return data;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
