fd96525c70e4b3c7b394536c1ac3843c3ee11cb1
Darya Korbut <kitkatlolipop@mail.ru>
2025-04-03 23:42:39 +0300

    feat: add remove btn and diolog to question card

---
+++ src/shared/components/question/components/confirm-remove-dialog/confirm-remove-dialog.types.ts
@@ -0,0 +1,5 @@
+export type ConfirmRemoveDialogProps = {
+  isOpen: boolean;
+  setIsOpen: (value: boolean) => void;
+  onRemove: () => void;
+};
--- src/shared/components/question/question.tsx
+++ src/shared/components/question/question.tsx
@@ -1,15 +1,55 @@
-import { Box, Card, CardContent, Chip, Paper, Typography } from '@mui/material';
+import { useState } from 'react';
+import { useNavigate, useParams } from 'react-router';
+import {
+  Box,
+  Card,
+  CardActions,
+  CardContent,
+  Chip,
+  IconButton,
+  Typography,
+} from '@mui/material';
+import { Delete as DeleteIcon } from '@mui/icons-material';
+import { Editor } from '@monaco-editor/react';
+import { useAppSelector } from '@app/store/hooks';
+import { useDeleteQuestion } from '@api/hooks';
 import { QuestionProps } from './question.types';
+import { ConfirmRemoveDialog } from './components';
+import styles from './styles.module.scss';
 
 export const Question = ({
+  id,
   description,
   title,
   isResolved,
   attachedCode,
   user,
 }: QuestionProps) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const navigate = useNavigate();
+  const { questionId } = useParams<{ questionId: string }>();
+  const { user: me } = useAppSelector((state) => state.user);
+  const {
+    deleteQuestion,
+    error: deleteError,
+    loading: deleteLoading,
+  } = useDeleteQuestion();
+
+  const handleRemove = async () => {
+    setIsOpen(false);
+    await deleteQuestion(id);
+
+    if (deleteError) return;
+
+    if (questionId) {
+      navigate('/questions');
+    } else {
+      navigate(0);
+    }
+  };
+
   return (
-    <Card>
+    <Card className={styles['question']} onClick={(e) => e.stopPropagation()}>
       <CardContent>
         <Box display="flex" justifyContent="space-between" alignItems="center">
           <Typography variant="h6" fontWeight="bold">
@@ -21,28 +61,44 @@ export const Question = ({
             size="small"
           />
         </Box>
-        <Typography variant="body2" color="text.secondary" mt={1}>
+        <Typography variant="body2" color="text.secondary" my={1}>
           {description}
         </Typography>
-        <Paper
-          variant="outlined"
-          sx={{
-            p: 1,
-            my: 2,
-            bgcolor: '#f5f5f5',
-            fontFamily: 'monospace',
-            whiteSpace: 'pre-wrap',
-            fontSize: 14,
+        <Editor
+          height="200px"
+          defaultValue={attachedCode}
+          theme="vs-dark"
+          options={{
+            readOnly: true,
           }}
-        >
-          {attachedCode}
-        </Paper>
+        />
         <Box sx={{ display: 'flex', alignItems: 'center', mt: 1 }}>
           <Typography variant="body2" fontWeight="bold">
             {user.username}
           </Typography>
         </Box>
+        <CardActions>
+          {me?.id === user.id && (
+            <IconButton
+              aria-label="Remove this post"
+              onClick={(e) => {
+                e.preventDefault();
+                setIsOpen(true);
+              }}
+              disabled={deleteLoading}
+              color="error"
+            >
+              <DeleteIcon />
+            </IconButton>
+          )}
+        </CardActions>
       </CardContent>
+
+      <ConfirmRemoveDialog
+        isOpen={isOpen}
+        setIsOpen={setIsOpen}
+        onRemove={handleRemove}
+      />
     </Card>
   );
 };
