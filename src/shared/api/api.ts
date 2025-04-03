7c4af4734fcfc66d12f15129c1e1e2039364d123
Darya Korbut <kitkatlolipop@mail.ru>
2025-04-03 13:34:05 +0300

    fix: add withCredentials prop to config api

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_BASE_URL + 'api',
+  baseURL: '/api',
+  withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
   },
