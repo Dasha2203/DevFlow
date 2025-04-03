4368f8b6ad9a9327f3f6fd32953e544b99a2aa05
Darya Korbut <kitkatlolipop@mail.ru>
2025-04-03 11:03:30 +0300

    fix: change PageTitle element

--- src/shared/ui/page-title/page-title.tsx
+++ src/shared/ui/page-title/page-title.tsx
@@ -1,15 +1,16 @@
 import clsx from 'clsx';
 import { Typography } from '@mui/material';
-import styles from './styles.module.scss';
 import { PageTitleProps } from './page-title.types';
+import styles from './styles.module.scss';
 
 export const PageTitle = ({ children, className }: PageTitleProps) => {
   return (
     <Typography
-      variant="h3"
+      variant="h4"
       component="h1"
       sx={{
         fontWeight: 'bold',
+        fontSize: { xs: '1.5rem', sm: '2rem', md: '2.5rem' },
       }}
       className={clsx(styles['title'], className)}
     >
