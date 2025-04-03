4eb36a9707192c7b031f2be49c5cf5d9ea1661fe
Darya Korbut <kitkatlolipop@mail.ru>
2025-04-03 12:18:53 +0300

    feat: add createPost page

---
+++ src/pages/create-post/create-post.tsx
@@ -0,0 +1,12 @@
+import { PostForm } from '@modules/post-form';
+import { PageTitle } from '@shared/ui';
+import styles from './styles.module.scss';
+
+export const CreatePost = () => {
+  return (
+    <>
+      <PageTitle className={styles['title']}>Create new post</PageTitle>
+      <PostForm />
+    </>
+  );
+};
