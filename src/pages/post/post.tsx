46d8d789796dea884e7cf43229d043ee75083ac8
Darya Korbut <kitkatlolipop@mail.ru>
2025-04-03 12:20:49 +0300

    feat: add Post page

---
+++ src/pages/post/post.tsx
@@ -0,0 +1,31 @@
+import { useParams } from 'react-router';
+import { Chip, CircularProgress, Typography } from '@mui/material';
+import { useSubscribePost } from '@api/hooks';
+import { CommentsList } from '@modules/comments-list';
+import { CreateCommentForm } from '@modules/create-comment-form';
+import { PostCard } from '@shared/components';
+import styles from './styles.module.scss';
+
+export const Post = () => {
+  const { postid } = useParams<{ postid: string }>();
+  const { post, loading } = useSubscribePost(postid);
+
+  if (loading) return <CircularProgress className={styles['loader']} />;
+
+  return (
+    <>
+      {post && <PostCard {...post} />}
+
+      <Typography variant="h5" component="p" sx={{ my: 4 }}>
+        Comments <Chip label={post?.comments.length} />
+      </Typography>
+
+      {post && <CreateCommentForm id={post.id} />}
+
+      <CommentsList
+        comments={post?.comments || []}
+        className={styles['list']}
+      />
+    </>
+  );
+};
